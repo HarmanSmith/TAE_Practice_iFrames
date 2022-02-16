2727321a1788583b41e5da371360bf1bf510a02b
HarmanSmith <leonel.diaz.16842@gmail.com>
2022-02-16 13:09:40 -0300

    access to first iframe

--- pages/iframes_page.py
+++ pages/iframes_page.py
@@ -16,19 +16,17 @@ class IframesPage:
         self.browser.get(self.url)
 
     #locators
-    iframe_locator = (By.ID, "thedynamichtml")
+    iframe_locator = ("thedynamichtml")
     frame1_item1_locator = (By.ID, "iframe0")
 
     def switch_to_iframe(self):
         # Store iframe web element
-        iframe = self.browser.find_element(self.iframe_locator)
         # switch to selected iframe
-        self.browser.switch_to.frame(iframe)
+        self.browser.switch_to.frame(self.iframe_locator)
 
     def check_iframe_item1(self):
         wait = WebDriverWait(self.browser, 10)
-        if wait.until(EC.visibility_of_element_located(self.frame1_item1_locator)):
-            return True
+        return wait.until(EC.visibility_of_element_located(self.frame1_item1_locator))
 
     # Now click on button
     # driver.find_element(By.TAG_NAME, 'button').click()
