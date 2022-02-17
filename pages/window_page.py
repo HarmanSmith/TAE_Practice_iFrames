3180e56578f23e90845d5e0d1e1120a4cf32a3c0
HarmanSmith <leonel.diaz.16842@gmail.com>
2022-02-17 16:15:09 -0300

    window test fixed

--- pages/window_page.py
+++ pages/window_page.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import time
 
 class WindowPage:
 
-    url = "http://www.learningaboutelectronics.com/Articles/How-to-open-a-new-window-in-Javascript.php"
+    url = "https://the-internet.herokuapp.com/windows"
 
     def __init__(self, browser):
         self.browser = browser
@@ -14,12 +14,14 @@ class WindowPage:
         self.browser.get(self.url)
 
     # locators
-    button_locator = (By.XPATH, "//*[@id='para1']/button")
+    button_locator = (By.XPATH, "//a[contains(text(), 'Click Here')]")
+    # //*[@id="content"]/div/a
 
     def open_new_window(self):
-        wait = WebDriverWait(self.browser, 30)
+        wait = WebDriverWait(self.browser, 120)
         wait.until(EC.visibility_of_element_located(self.button_locator)).click()
-        wait.until(EC.number_of_windows_to_be(2))
+        # wait.until(EC.number_of_windows_to_be(2))
+        # number_of_windows includes number of tabs?
 
     def get_window_handle(self):
         # wait = WebDriverWait(self.browser, 10)
@@ -35,15 +37,20 @@ class WindowPage:
     def switch_to_new_window(self):
         # Loop through until we find a new window handle
         # original_window = self.browser.current_window_handle()
-        # todo here is where the test fails NoSuchWindow Exception
-        # for some reason the index doesnt work either in int or string format
-        self.browser.switch_to.window("1")
+        # chrome opens a new tab instead of a new window
+        # todo: window or tab, neither are accessible with this command for some reason
+        original_window = self.browser.current_window_handle
+        for window_handle in self.browser.window_handles:
+            if window_handle != original_window:
+                self.browser.switch_to.window(window_handle)
+                break
+        # self.browser.switch_to.window("")
 
     def close_window(self):
         self.browser.close()
 
     def check_windows_opened(self, index):
-        wait = WebDriverWait(self.browser, 10)
+        # wait = WebDriverWait(self.browser, 10)
         if len(self.browser.window_handles) == index:
             return True
         else:
